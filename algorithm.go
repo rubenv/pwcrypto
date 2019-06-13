ed5fd874dbebfd2507cbafd2a670671641681c6e
Ruben Vermeersch <ruben@rocketeer.be>
2019-06-13 09:03:40 +0200

    Initial implementation with null crypto

---
+++ algorithm.go
@@ -0,0 +1,22 @@
+package backend
+
+type Algorithm interface {
+	ID() string
+	Hash(input string) (string, error)
+	Check(input, hashed string) (bool, error)
+}
+
+type NullCrypto struct {
+}
+
+func (n NullCrypto) ID() string {
+	return "null"
+}
+
+func (n NullCrypto) Hash(input string) (string, error) {
+	return input, nil
+}
+
+func (n NullCrypto) Check(input, hashed string) (bool, error) {
+	return input == hashed, nil
+}
---
+++ crypto.go
@@ -0,0 +1,55 @@
+package backend
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Crypto struct {
+	preferred  string
+	algorithms []Algorithm
+}
+
+func New(algorithms ...Algorithm) *Crypto {
+	preferred := ""
+	if len(algorithms) > 0 {
+		preferred = algorithms[0].ID()
+	}
+
+	return &Crypto{
+		preferred:  preferred,
+		algorithms: algorithms,
+	}
+}
+
+func (c *Crypto) Hash(input string) (string, error) {
+	if len(c.algorithms) == 0 {
+		return "", errors.New("No password crypto algorithms defined!")
+	}
+
+	alg := c.algorithms[0]
+	hashed, err := alg.Hash(input)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s|%s", alg.ID(), hashed), nil
+}
+
+func (c *Crypto) Check(input, hashed string) (valid bool, mustUpgrade bool, err error) {
+	parts := strings.SplitN(hashed, "|", 2)
+
+	for _, a := range c.algorithms {
+		if a.ID() == parts[0] {
+			valid, err = a.Check(input, hashed)
+			if err != nil {
+				return false, false, err
+			}
+
+			mustUpgrade = a.ID() != c.preferred
+			return valid, mustUpgrade, nil
+		}
+	}
+
+	return false, false, nil
+}
