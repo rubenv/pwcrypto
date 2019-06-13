1c7228ddbfe1b1862123fef3a3372d257fd3920f
Ruben Vermeersch <ruben@rocketeer.be>
2019-06-13 09:03:55 +0200

    Add unit test

---
+++ crypto_test.go
@@ -0,0 +1,29 @@
+package backend
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNullCrypto(t *testing.T) {
+	runCryptoTests(t, NullCrypto{})
+}
+
+func runCryptoTests(t *testing.T, a Algorithm) {
+	assert := assert.New(t)
+
+	assert.NotEqual("", a.ID())
+
+	h, err := a.Hash("test")
+	assert.NoError(err)
+	assert.NotEqual("", h)
+
+	ok, err := a.Check("test", h)
+	assert.NoError(err)
+	assert.True(ok)
+
+	ok, err = a.Check("bad", h)
+	assert.NoError(err)
+	assert.False(ok)
+}
